97531e42e3e062bf4d49e4d9a5949662014c6b6d
Umangraj04 <165834791+Umangraj04@users.noreply.github.com>
2024-12-02 20:14:14 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5000/api/v1" || "https://doctor-appointment-amber-chi.vercel.app" ;
+export const BASE_URL = "http://localhost:5000/api/v1"  ;
 
 export const token = localStorage.getItem('token');
