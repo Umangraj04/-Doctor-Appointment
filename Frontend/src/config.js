0b1191bb8c456001d1da43871b7e49b8159aab04
Umangraj04 <165834791+Umangraj04@users.noreply.github.com>
2024-12-08 12:40:28 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://doctor-appointment-back.onrender.com"
+export const BASE_URL = "https://doctor-appointment-back.onrender.com/api/v1"
 
 export const token = localStorage.getItem('token');
